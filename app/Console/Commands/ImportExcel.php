18ebfef9fead8ba480ef83b5595080bc3339fefe
root <root@DESKTOP-IDL0PMD>
2024-04-21 22:53:45 +0100

    Create the console command for importing the podfather customer data from an excel file.

--- app/Console/Commands/ImportExcel.php
+++ app/Console/Commands/ImportExcel.php
@@ -1,8 +1,11 @@
 <?php
 
+
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Imports\CustomerWasteImport; // This will be your import class
 
 class ImportExcel extends Command
 {
@@ -11,20 +14,40 @@ class ImportExcel extends Command
      *
      * @var string
      */
-    protected $signature = 'app:import-excel';
+    protected $signature = 'import:excel {file}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Imports data from an Excel file into the database';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
     /**
      * Execute the console command.
+     *
+     * @return int
      */
     public function handle()
     {
-        //
+        $file = $this->argument('file'); // Get the file argument from the command line
+
+        if (!file_exists($file)) {
+            $this->error("The file {$file} does not exist.");
+            return;
+        }
+
+        try {
+            Excel::import(new CustomerWasteImport, $file);
+            $this->info('Data has been successfully imported.');
+            return Command::SUCCESS;
+        } catch (\Exception $e) {
+            $this->error("There was an error importing the data: " . $e->getMessage());
+        }
     }
 }
